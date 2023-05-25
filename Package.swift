d29747f7e726db6fde19ca7f86b19c22c14d31d5
Tony Tuong <tuong.dang@c0x12c.com>
2023-05-25 17:31:33 +0700

    Fix SPM and clean sources

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version:5.4
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Wormholy",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Wormholy",
             targets: ["Wormholy"]),
@@ -15,10 +13,9 @@ let package = Package(
     targets: [
         .target(
             name: "Wormholy",
-            path: "Source",
-            dependencies: []),
-        .testTarget(
-            name: "WormholyTests",
-            dependencies: ["Wormholy"]),
+            path: "Sources",
+            exclude: [],
+            resources: [.process("Resources")]
+        )
     ]
 )
